0bd58ad78bd810c761ccb2b0dbbf80831845b546
RobThree <rob@devcorner.nl>
2021-12-06 17:48:24 +0100

    Add wait for birthday

---
+++ include/example_config.h
@@ -0,0 +1,10 @@
+// COPY THIS FILE AND RENAME IT TO "config.h" AND ENTER ALL YOUR CONFIGURATION DATA BELOW
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Replace with your actual SSID and password:
+#define WIFI_SSID       "***SSID***"
+#define WIFI_PASSWD     "***WIFIPASSWORD***"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,44 @@
 #include <Arduino.h>
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
+#include <config.h>
 
 const double SONGDURATION = 15.9;  // Seconds
 const uint8_t CARDPIN = D4;
+const unsigned long BIRTHDAY = 1639378800; // Monday, December 13, 2021 8:00:00 AM GMT+01:00
+const unsigned long DEEPSLEEP = 300;  // Seconds
+
+WiFiEventHandler gotIpEventHandler, disconnectedEventHandler;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+
+void setupWiFi() {
+  // Start WiFi
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+
+  disconnectedEventHandler = WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected& event)
+  {
+    Serial.println("Disconnected!");
+    WiFi.reconnect();
+  });
+
+  gotIpEventHandler = WiFi.onStationModeGotIP([](const WiFiEventStationModeGotIP& event)
+  {
+    Serial.printf("Got IP: %s\n", event.ip.toString().c_str());
+  });
+
+}
+
+void startWiFi() {
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
 
 void sleep(double seconds) {
-  //ESP.deepSleep(seconds * 1000 * 1000, RF_DISABLED);
   delay(seconds * 1000);
 }
 
@@ -18,9 +52,24 @@ void setup() {
   // Set PIN low (i.e. disable the bday card player) ASAP
   pinMode(CARDPIN, OUTPUT);
   digitalWrite(CARDPIN, LOW);
+  pinMode(D0, WAKEUP_PULLUP);
+
+  Serial.begin(460800);
+  setupWiFi();
+  startWiFi();
+
+  timeClient.begin();
 }
 
 void loop() {
-  sing();
-  sleep(random(300));
+  timeClient.update();
+  unsigned long currenttime = timeClient.getEpochTime();
+  if (currenttime > BIRTHDAY) { 
+    Serial.println("Yay!");
+    sing();
+    sleep(random(300));
+  } else {
+    Serial.printf("Nope. At least %lu seconds left. Snoozing for %lu seconds\n", BIRTHDAY - currenttime, DEEPSLEEP);
+    sleep(DEEPSLEEP); // ESP.deepsleep won't work for some reason. Screw it, using regular sleep.
+  }
 }
